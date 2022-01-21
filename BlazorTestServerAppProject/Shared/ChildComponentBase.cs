c5c727eb389de8d0a41e3c8e2d116360fde4b106
mehdi <0r0.developer@gmail.com>
2022-01-22 02:23:49 +0400

    add all  @Code block code in ChildComponent.razor to ChildComponentBase.cs and then add @inherits to razor file

--- BlazorTestServerAppProject/Shared/ChildComponentBase.cs
+++ BlazorTestServerAppProject/Shared/ChildComponentBase.cs
@@ -1,6 +1,16 @@
-﻿namespace BlazorTestServerAppProject.Shared
+﻿using Microsoft.AspNetCore.Components;
+namespace BlazorTestServerAppProject.Shared
 {
-    public class ChildComponentBase
+    public class ChildComponentBase :ComponentBase
     {
+        protected bool DarkThemeOn;
+        protected string AlertTheme => DarkThemeOn ? "dark" : "light";
+        [Parameter]
+        public RenderFragment childContent { get; set; }
+
+        protected override void OnInitialized()
+        {
+            DarkThemeOn = true;
+        }
     }
 }
