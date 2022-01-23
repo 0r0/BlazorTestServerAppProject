beca183b6cf3ececac517a6a1b71aad161a76821
mehdi <0r0.developer@gmail.com>
2022-01-23 17:07:35 +0400

    add setElementTextById and  focusOnElement functions

--- BlazorTestServerAppProject/wwwroot/js/interop.js
+++ BlazorTestServerAppProject/wwwroot/js/interop.js
@@ -5,3 +5,12 @@
 function createPrompt(question) {
     return prompt(question);
 }
+
+function setElementTextById(id, text) {
+    document.getElementById(id).innerText = text;
+
+}
+
+function focusOnElement(element) {
+    element.focus();
+}
