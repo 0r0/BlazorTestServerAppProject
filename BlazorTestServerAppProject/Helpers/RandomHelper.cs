f3b77b5bae51aceda69de8395198e82f0b5704db
mehdi <0r0.developer@gmail.com>
2022-01-24 11:40:21 +0400

    create Random Helper class that js invokable method put it

---
+++ BlazorTestServerAppProject/Helpers/RandomHelper.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.JSInterop;
+
+namespace BlazorTestServerAppProject.Helpers
+{
+    public class RandomHelper
+    {
+        [JSInvokable]
+        public static Task<int> GenerateRandomInt(int maxValue)
+        {
+            return Task.FromResult(new Random().Next(maxValue));
+        }
+    }
+}
