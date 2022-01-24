16f213f0212339cb789f6c3823302efd8bbecc7b
mehdi <0r0.developer@gmail.com>
2022-01-24 11:39:22 +0400

    add two methods that generate random integer in 2 ways

--- BlazorTestServerAppProject/wwwroot/js/interop.js
+++ BlazorTestServerAppProject/wwwroot/js/interop.js
@@ -14,3 +14,13 @@ function setElementTextById(id, text) {
 function focusOnElement(element) {
     element.focus();
 }
+function giveMeRandomInt(maximumValue) {
+    DotNet.invokeMethodAsync('BlazorTestServerAppProject', 'GenerateRandomInt', maximumValue)
+        .then(result => setElementTextById('randomNumberSpan', result))
+}
+
+function giveMeRandomInt2(maximumValue2,dotnetInstance) {
+
+    dotnetInstance.invokeMethodAsync('GenerateRandomInt', maximumValue2).then(result => {
+        setElementTextById('randomNumberSpan2', Math.floor(Math.random(maximumValue2)))});
+}
