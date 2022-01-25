d893016ffbd5e4280739d45ea1098e021b07543a
mehdi <0r0.developer@gmail.com>
2022-01-25 23:53:45 +0400

    add validation and field in JobApplication class

--- BlazorTestServerAppProject/Data/JobApplication.cs
+++ BlazorTestServerAppProject/Data/JobApplication.cs
@@ -1,6 +1,22 @@
-﻿namespace BlazorTestServerAppProject.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorTestServerAppProject.Data
 {
     public class JobApplication
     {
+        [Required]
+        public string FullName { get; set; }
+        [StringLength(500,ErrorMessage="you can not put so many words here")]
+        public string Description { get; set; }
+        [Required]
+        [Range(1000000,50000000,ErrorMessage ="select salary between one million to 50 millions")]
+        public int SalaryExpectation { get; set; }
+        [Required]
+        public bool DoesOpenSource { get; set; }
+        [Required]
+        public DateTime Availability { get; set; }  
+
+
+
     }
 }
