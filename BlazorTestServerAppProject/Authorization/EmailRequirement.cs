68e57d5cbd0673610eabb3708535b9579ad7347d
mehdi <0r0.developer@gmail.com>
2022-01-31 21:53:33 +0400

    create email requirement class for auth

---
+++ BlazorTestServerAppProject/Authorization/EmailRequirement.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace BlazorTestServerAppProject.Authorization
+{
+    public class EmailRequirement:IAuthorizationRequirement
+    {
+        public string EmailSuffix { get;  }
+        public EmailRequirement(string EmailSuffix)
+        {
+            this.EmailSuffix = EmailSuffix;
+        }
+    }
+}
