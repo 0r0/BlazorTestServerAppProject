a36cf4f52f4c87c64d6f75b90740f7fb6e3246c9
mehdi <0r0.developer@gmail.com>
2022-01-31 21:54:24 +0400

    add Email Auth Handler class for handling auth

---
+++ BlazorTestServerAppProject/Authorization/EmailAuthHandler.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace BlazorTestServerAppProject.Authorization
+{
+    public class EmailAuthHandler : AuthorizationHandler<EmailRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, EmailRequirement requirement)
+        {
+            if (context.User.Identity.Name.EndsWith(requirement.EmailSuffix))
+            {
+                context.Succeed(requirement);
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
